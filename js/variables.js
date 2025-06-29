0cc5dd94eaed027ff4596267930b834c8ca1a534
Shruti <sg162222@gmail.com>
2025-06-29 12:20:28 +0530

    Keep only digitalClock folder in GitHub

--- js/variables.js
+++
@@ -1,8 +0,0 @@
-// console.log("Shruti");
-let user={name: "Shruti", age: 24};
-// console.log(user.name);console.log(user.age);
-const accountPassword="shruti1622";
-let accountState="Bengaluru";
-// console.log(accountPassword);
-// console.log(accountState);
-console.table([accountPassword, accountState]);
